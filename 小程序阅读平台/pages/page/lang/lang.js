d280477469777bd20bffdeb5a3e95eca6c6cf5d7
aboutus2x2 <tian755688wang449@163.com>
2025-05-10 03:42:37 +0800

    update

---
+++ 小程序阅读平台/pages/page/lang/lang.js
@@ -0,0 +1,87 @@
+// pages/page/lang/lang.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    user: {
+      name: '张三',
+      age: 17,
+      sex: 'other'
+    },
+    videos: [
+      {
+        id: 15,
+        title: '这是我的桌面',
+        img: '/img/desktop.png'
+      },
+      {
+        id: 17,
+        title: '国际友好手势',
+        img: '/img/hand.png'
+      },
+      {
+        id: 89,
+        title: '一封情书',
+        img: '/img/letter.png'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
