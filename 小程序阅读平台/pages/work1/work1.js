4810d9898a3dd55f20bedf2a98db86a8739dc21e
aboutus2x2 <tian755688wang449@163.com>
2025-06-06 20:29:37 +0800

    update

---
+++ 小程序阅读平台/pages/work1/work1.js
@@ -0,0 +1,74 @@
+// pages/work1/work1.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 设置颜色值
+    color: '#000'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  onToggle() {
+    // 要更新页面的话就必须使用 this.setData 方法
+    this.setData({
+      color: this.data.color === '#000'? '#ff0': '#000'
+    })
+  }
+})
