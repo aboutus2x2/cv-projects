d57f5a65849a3d5cd52c28e62ac7cb4baebe5737
aboutus2x2 <tian755688wang449@163.com>
2025-06-15 06:35:57 +0800

    update

---
+++ 小程序阅读平台/pages/event/event.js
@@ -0,0 +1,86 @@
+// pages/event/event.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  onBind1() {
+    console.log('bind 1');
+  },
+
+  onBind2() {
+    console.log('bind 2');
+  },
+
+  onCatch2() {
+    console.log('catch 2');
+  },
+
+  onCaptureBind1() {
+    console.log('capture-bind 1');
+  },
+
+  onCaptureCatch1() {
+    console.log('capture-catch 1');
+  }
+})
