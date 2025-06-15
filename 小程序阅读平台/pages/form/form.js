cee43ec3ed8944cc8511cb9fb21c47fb1433993b
aboutus2x2 <tian755688wang449@163.com>
2025-06-16 01:14:27 +0800

    update

---
+++ 小程序阅读平台/pages/form/form.js
@@ -0,0 +1,141 @@
+// pages/form/form.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: '李四',
+    pwd: '123',
+    sex: 'other',
+    // 复选按钮的数据源
+    hobbies: [{
+        value: 'dlq',
+        text: '打篮球',
+        checked: false
+      },
+      {
+        value: 'tzq',
+        text: '踢足球',
+        checked: true
+      },
+      {
+        value: 'ymq',
+        text: '羽毛球',
+        checked: true
+      },
+    ],
+    agree: false,
+    desc: '123\n456'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  // 姓名输入事件
+  onNameInput(ev) {
+    console.log(ev);
+
+    this.setData({
+      name: ev.detail.value
+    })
+  },
+
+  onPwdInput(ev) {
+    this.setData({
+      pwd: ev.detail.value
+    })
+  },
+
+  // 单选按钮变化事件
+  onSexChange(ev) {
+    console.log(ev);
+    this.setData({
+      sex: ev.detail.value
+    })
+  },
+
+  // 复选框变化事件
+  onHobbiesChange(ev) {
+    console.log(ev);
+    // 当前选中的值数组
+    ev.detail.value
+    this.data.hobbies.forEach(hobby => {
+      // 若选项包含在ev.detail.value中，则修改checked为true
+      hobby.checked = ev.detail.value.includes(hobby.value)
+    })
+    this.setData({
+      hobbies: this.data.hobbies
+    })
+  },
+
+  onAgreeChange(ev) {
+    console.log(ev);
+    this.setData({
+      agree: ev.detail.value
+    })
+  },
+
+  onDescInput(ev) {
+    this.setData({
+      desc: ev.detail.value
+    })
+  },
+
+  onSubmit() {
+    console.log(this.data);
+  }
+})
