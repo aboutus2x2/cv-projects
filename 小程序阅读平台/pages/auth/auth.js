9b8f1baeccfc62f0adc27f5ea16a17e6a048bbdd
aboutus2x2 <tian755688wang449@163.com>
2025-06-25 06:33:07 +0800

    update

--- 小程序阅读平台/pages/auth/auth.js
+++ 小程序阅读平台/pages/auth/auth.js
@@ -66,5 +66,66 @@ Page({
 
   takePhoto() {
     console.log('takePhoto');
+
+    // // 读取用户当前权限
+    // wx.getSetting({
+    //   success: res => {
+    //     console.log(res);
+
+    //     if (!res.authSetting['scope.camera']) {
+    //       // 若不存在相机权限则进行授权
+    //       wx.authorize({
+    //         scope: 'scope.camera',
+    //         success: res => {
+    //           console.log(res);
+    //           console.log('相机权限授权成功');
+
+    //           // 授权相机权限成功后就可以调用相机对应的功能
+    //           // 比如 拍照
+    //           console.log('拍照成功');
+    //         },
+    //         fail: err => {
+    //           console.log(err);
+    //         }
+    //       })
+    //     } else {
+    //       // 已经有权限时
+    //       // 直接调用对应权限的api
+    //       console.log('拍照成功');
+    //     }
+    //   }
+    // })
+
+
+    // 读取用户权限
+    wx.getSetting({
+      success: res => {
+        console.log(res);
+
+        if (typeof res.authSetting['scope.camera'] === 'undefined') {
+          // 用户从未授权过
+
+          // 进行授权
+          wx.authorize({
+            scope: 'scope.camera',
+            success: () => {
+              // 授权成功
+              // 进行拍照
+              console.log('拍照成功');
+            }
+          })
+        } else if (res.authSetting['scope.camera'] === false) {
+          // 授权的时候拒绝过
+
+          // 打开设置项让用户自己开启权限
+          wx.openSetting()
+        } else {
+          // 已经授权过
+
+          // 进行拍照
+          console.log('拍照成功');
+        }
+      }
+    })
   }
 })
