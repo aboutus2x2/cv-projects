dba13674e7ae09e0d3f29a7db2310b33d51d654d
aboutus2x2 <tian755688wang449@163.com>
2025-06-27 22:34:27 +0800

    update

---
+++ 小程序阅读平台/pages/screen/screen.js
@@ -0,0 +1,83 @@
+// pages/screen/screen.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 状态栏高度
+    statuBarHeight: 0,
+    // 选项卡底部占位组件的高度
+    tabbarBottom: 0,
+    // 底部占位符高度
+    bottomPlaceholder: 0,
+    // 滚动屏的高度
+    scrollViewHeight: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    // 获取屏幕设备的参数
+    const windowInfo = wx.getWindowInfo()
+    console.log(windowInfo);
+    this.setData({
+      // 获取安全区域的顶部高度并赋值给顶部占位符
+      statuBarHeight: windowInfo.safeArea.top,
+      tabbarBottom: windowInfo.screenHeight - windowInfo.safeArea.bottom,
+      bottomPlaceholder: windowInfo.screenHeight - windowInfo.safeArea.bottom + 50,
+      scrollViewHeight: windowInfo.screenHeight - windowInfo.safeArea.top - 50 - (windowInfo.screenHeight - windowInfo.safeArea.bottom + 50),
+      triggered: false,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    console.log('开启下拉刷新');
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
