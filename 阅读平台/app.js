cc5a5e5c234342788f19ae520297518fe39b3a12
aboutus2x2 <tian755688wang449@163.com>
2025-07-12 09:28:57 +0800

    update

--- 阅读平台/app.js
+++ 阅读平台/app.js
@@ -1,17 +1,26 @@
+// 导入leanCloud sdk
+const AV = require('./libs/av-core-min.js');
+const adapters = require('./libs/leancloud-adapters-weapp.js');
+
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+
+    // 安装微信的适配器
+    AV.setAdapters(adapters);
+    
+    // 初始化 leanCloud sdk
+    AV.init({
+      appId: "UrSuYRev392dvKthkmVVzj7K-gzGzoHsz",
+      appKey: "G5c7JhuaIUQAGqpGTXe7ZNWX",
+      serverURL: "https://ursuyrev.lc-cn-n1-shared.com"
+    });
   },
   globalData: {
     userInfo: null
