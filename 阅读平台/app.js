c5fdf5ccaabd45d0ca0f33b11e14a4986db3e7e3
aboutus2x2 <tian755688wang449@163.com>
2025-07-13 03:51:17 +0800

    update

--- 阅读平台/app.js
+++ 阅读平台/app.js
@@ -14,13 +14,20 @@ App({
 
     // 安装微信的适配器
     AV.setAdapters(adapters);
-    
+
     // 初始化 leanCloud sdk
     AV.init({
       appId: "UrSuYRev392dvKthkmVVzj7K-gzGzoHsz",
       appKey: "G5c7JhuaIUQAGqpGTXe7ZNWX",
       serverURL: "https://ursuyrev.lc-cn-n1-shared.com"
     });
+
+    // 测试读取数据
+    const query = new AV.Query('Test')
+    // 调用find 查询数据
+    query.find().then(res => {
+      console.log(res);
+    })
   },
   globalData: {
     userInfo: null
