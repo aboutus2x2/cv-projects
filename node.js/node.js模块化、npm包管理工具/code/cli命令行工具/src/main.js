e7e1ac77df8311da368c3597df5d8009ed7547d9
aboutus2x2 <tian755688wang449@163.com>
2026-04-10 04:06:51 +0800

    update

---
+++ node.js/node.js模块化、npm包管理工具/code/cli命令行工具/src/main.js
@@ -0,0 +1,14 @@
+console.log('hello world')
+console.log(process.argv)
+const args = process.argv.slice(2)
+let a = 0
+let b = 0
+// 解析参数
+for (let i = 0; i < args.length; i++) {
+    const arg =  args[i]
+    const splits = arg.split('=')
+    if(splits[0] === 'a') a = Number(splits[1])
+    else if(splits[0] === 'b') b = Number(splits[1])
+}
+
+console.log(a + b)
