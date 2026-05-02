af788eddbf06aa3985d8ca8389c7ad69f9fc6e29
aboutus2x2 <tian755688wang449@163.com>
2026-05-03 05:09:11 +0800

    update

--- node.js/express基础篇/project/搭建express服务器/ajax服务器.js
+++ node.js/express基础篇/project/搭建express服务器/ajax服务器.js
@@ -5,6 +5,12 @@ const port = 80
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+// 解析post请求的参数
+// 用来解析数据类型为 application/json 格式的数据
+app.use(express.json())
+// 用来解析数据类型为 application/x-www-form-urlencoded 格式的数据
+app.use(express.urlencoded({extended: true}))
+
 app.get('/say', (req, res) => {
     res.json({msg: 'hello'})
 })
@@ -13,6 +19,17 @@ app.get('/add', (req, res) => {
     // get 请求的参数可以直接通过 req.query 获取
     // res.json(req.query)
     res.json({result: Number(req.query.a) + Number(req.query.b)})
+})
+
+app.post('/postJson', (req, res) => {
+    // post 请求依然可以接收查询字符串参数
+    console.log(req.query)
+    // post请求使用body接收参数
+    res.json(req.body)
+})
+
+app.post('/postQs', (req, res) => {
+    res.json(req.body)
 })
 
 app.listen(port, () => {
