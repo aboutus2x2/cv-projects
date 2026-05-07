6dfc4d5a39cde5b1ae2e55654a5515ae85a785bd
aboutus2x2 <tian755688wang449@163.com>
2026-05-07 20:24:31 +0800

    update

--- node.js/express基础篇/project/搭建express服务器/ajax服务器.js
+++ node.js/express基础篇/project/搭建express服务器/ajax服务器.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fileUpload = require('express-fileupload')
+const fsp = require('fs/promises')
 const path = require("path");
 const app = express();
 const port = 80
@@ -10,6 +12,16 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 // 用来解析数据类型为 application/x-www-form-urlencoded 格式的数据
 app.use(express.urlencoded({extended: true}))
+// 解析文件参数的配置
+app.use(fileUpload({
+    // 文件大小限制
+    limits: {fileSize: 10 * 1024 * 1024},
+    // 使用临时文件
+    useTempFiles: true,
+    // 临时文件夹路径
+    tempFileDir: path.join(__dirname, 'tmp')
+}))
+
 
 app.get('/say', (req, res) => {
     res.json({msg: 'hello'})
@@ -30,6 +42,21 @@ app.post('/postJson', (req, res) => {
 
 app.post('/postQs', (req, res) => {
     res.json(req.body)
+})
+
+// 上传文件
+app.post('/upload', async (req, res) => {
+    // 读取文件参数
+    // req.files 代表上传的文件集合
+    console.log(req.files.f)
+    let file = req.files.f
+    // 读取临时文件
+    const data = await fsp.readFile(file.tempFilePath)
+    // 写入上传文件的文件夹
+    await fsp.writeFile(path.join(__dirname, 'upload', file.name), data)
+    // 删除临时文件
+    await fsp.rm(file.tempFilePath, {force: true})
+    res.json({msg: 'ok'})
 })
 
 app.listen(port, () => {
