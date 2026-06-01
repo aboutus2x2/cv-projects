a1be51f5ebcd773d669a8476f5e6f0ad2d55c800
aboutus2x2 <tian755688wang449@163.com>
2026-06-02 03:21:12 +0800

    update

--- node.js/ejs模板引擎/project/node.js中调用ejs模板引擎.js
+++ node.js/ejs模板引擎/project/node.js中调用ejs模板引擎.js
@@ -4,17 +4,25 @@ const path = require("path");
 // 1. 没有模板文件时，通过字符串模板编译html
 
 // 通过字符串模板，构造一个模板对象
-const template = ejs.compile(`<p><%=name%></p>`)
-// 调用模板来输出html
-let html = template({name: '隔壁老王'})
-console.log(html)
+// const template = ejs.compile(`<p><%=name%></p>`)
+// // 调用模板来输出html
+// let html = template({name: '隔壁老王'})
+// console.log(html)
+//
+// // 2. 合并创建模板对象和输出模板的过程，直接调用 render 来渲染模板
+// html = ejs.render(`<p><%=name%></p>`, {name: '法外狂徒张三'})
+// console.log(html)
+//
+// // 3. 通过模板文件渲染html
+// let promise = ejs.renderFile(path.join(__dirname, 'template.ejs'), {user: {name: '张三', sex: 'male', age: 17}})
+// promise.then(html => {
+//     console.log(html)
+// })
 
-// 2. 合并创建模板对象和输出模板的过程，直接调用 render 来渲染模板
-html = ejs.render(`<p><%=name%></p>`, {name: '法外狂徒张三'})
-console.log(html)
-
-// 3. 通过模板文件渲染html
-let promise = ejs.renderFile(path.join(__dirname, 'template.ejs'), {user: {name: '张三', sex: 'male', age: 17}})
-promise.then(html => {
+ejs.renderFile(path.join(__dirname, '模板语法.ejs'), {name: '老王', sex: 'other', users: [
+        {name: '张三', age: 17},
+        {name: '李四', age: 24},
+        {name: '老王', age: 30},
+    ], child: '子模板中的name'}).then(html=>{
     console.log(html)
 })
